8813f87b302e6d213f3777ac8cd31d9300677b1f
Matias Casiba <casibagabriel@gmail.com>
2025-10-17 17:59:58 -0300

    Armo las operaciones que debe tener mi calculadora dentro de OperacionesMatematicas.java

--- src/calculadora/modelo/OpercionesMatematicas.java
+++ src/calculadora/modelo/OpercionesMatematicas.java
@@ -1,5 +1,54 @@
 package calculadora.modelo;
 
 public class OpercionesMatematicas {
+	
+	public static double sumar(double a, double b) {
+		return a + b;
+	}
+	
+	public static double restar(double a, double b) {
+		return a - b;
+	}
+	
+	public static double multiplicar(double a, double b) {
+		return a * b;
+	}
+	
+	public static double dividir(double a, double b) {
+		if(b==0) {
+			throw new IllegalArgumentException("No se puede dividir por cero");
+		}
+		return a / b;
+	}
+	
+	public static double potencia(double a, double b) {
+		return Math.pow(a, b);
+	}
+	
+	public static double seno(double a) {
+		return Math.sin(a);
+	}
+	
+	public static double coseno(double a) {
+		return Math.cos(a);
+	}
+	
+	public static double logaritmo(double a) {
+		if(a <= 0) {
+			throw new IllegalArgumentException("No se puede calcular");
+		}
+		return Math.log(a);
+	}
+	
+	public static int factorial(int n) {
+		if(n < 0) {
+			throw new IllegalArgumentException("No se puede calcular");
+		}
+		int resultado = 1;
+		for(int i = 2; i <= n; i++) {
+			resultado *= i;
+		}
+		return resultado;
+	}
 
 }
