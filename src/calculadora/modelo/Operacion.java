3e2054d1561fe5dd4e0047f8061cbf9e134c960a
Matias Casiba <casibagabriel@gmail.com>
2025-10-18 11:20:23 -0300

    Armo el método calcular() en Operacion.java

--- src/calculadora/modelo/Operacion.java
+++ src/calculadora/modelo/Operacion.java
@@ -10,4 +10,41 @@ public class Operacion {
 		this.operandos = operandos.clone();
 		
 	}
+	
+	public double calcular() {
+		switch(nombre) {
+			case "suma":
+				resultado = OpercionesMatematicas.sumar(operandos[0], operandos[1]);
+				break;
+			case "resta":
+				resultado = OpercionesMatematicas.restar(operandos[0], operandos[1]);
+				break;
+			case "multiplicacion":
+				resultado = OpercionesMatematicas.multiplicar(operandos[0], operandos[1]);
+				break;
+			case "division":
+				resultado = OpercionesMatematicas.dividir(operandos[0], operandos[1]);
+				break;
+			case "potencia":
+				resultado = OpercionesMatematicas.potencia(operandos[0], operandos[1]);
+				break;
+			case "raiz":
+				resultado = OpercionesMatematicas.raiz(operandos[0]);
+				break;
+			case "seno":
+				resultado = OpercionesMatematicas.seno(operandos[0]);
+				break;
+			case "coseno":
+				resultado = OpercionesMatematicas.coseno(operandos[0]);
+			case "logaritmo":
+				resultado = OpercionesMatematicas.logaritmo(operandos[0]);
+				break;
+			case "factorial":
+				resultado = OpercionesMatematicas.factorial((int) operandos[0]);
+				break;
+			default:
+				throw new IllegalArgumentException("Operación desconocida: " + nombre);
+		}
+		return resultado;
+	}
 }
