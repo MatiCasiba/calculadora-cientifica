f7ae310fc1c9b235d247068a5f78240b7b99e98b
Matias Casiba <casibagabriel@gmail.com>
2025-10-18 11:06:16 -0300

    Trabajo en la clase Operacion.java (Creo el constructor)

--- src/calculadora/modelo/Operacion.java
+++ src/calculadora/modelo/Operacion.java
@@ -1,5 +1,13 @@
 package calculadora.modelo;
 
 public class Operacion {
-
+	private String nombre;
+	private double[] operandos;
+	private double resultado;
+	
+	public Operacion(String nombre, double... operandos) {
+		this.nombre = nombre.toLowerCase().trim();
+		this.operandos = operandos.clone();
+		
+	}
 }
