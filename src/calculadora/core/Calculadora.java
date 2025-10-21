f4e9edced166322f80600ef3c4165eaf743f887e
Matias Casiba <casibagabriel@gmail.com>
2025-10-21 18:39:02 -0300

    Armo el constructor de Calculadora

--- src/calculadora/core/Calculadora.java
+++ src/calculadora/core/Calculadora.java
@@ -1,5 +1,17 @@
 package calculadora.core;
 
-public class Calculadora {
+import calculadora.modelo.OpercionesMatematicas;
 
+public class Calculadora {
+	private double pantalla;
+	private Memoria memoria;
+	private HistorialOperaciones historial;
+	private OpercionesMatematicas operaciones;
+	
+	public Calculadora() {
+		this.pantalla = 0.0;
+		this.memoria = new Memoria();
+		this.historial = new HistorialOperaciones();
+		this.operaciones = new OpercionesMatematicas();
+	}
 }
